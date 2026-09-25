435a87deda5b8d69efd8e7456c314bd95fed063a
Livingstone Maganya <livingstoneowiny@gmail.com>
2026-09-25 19:28:58 +0300

    Configure Express app for Vercel

--- server.js
+++ server.js
@@ -186,6 +186,11 @@ app.delete(
 );
 
 app.use(errorHandler);
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost: ${PORT}`)
-);
+
+if (!process.env.VERCEL) {
+  app.listen(PORT, () =>
+    console.log(`Server running on http://localhost: ${PORT}`)
+  );
+}
+
+export default app;
